9f9a724cd164e7edf92c42409b1b95a41cdbac2a
Grantsullens <sullens.ga@gmail.com>
2024-10-24 01:02:10 +0000

    Final

--- dice.rb
+++ dice.rb
@@ -20,44 +20,57 @@ get("/giraffe") do
 end
 # Dice 2/6
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+  @rolls = [] 
+  2.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+ 
   erb(:two_six)
 end
 # Dice 2/10
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = [] 
+  2.times do    # 100 times...
+    die = rand(1..10)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
 	
   erb(:two_ten)
 end
 # Dice 1/20
 get("/dice/1/20") do
-  first_die = rand(1..20)
-  sum = first_die
-	
-  @outcome = "You rolled a #{first_die} for a total of #{sum}."
-	
+  @rolls = [] 
+  1.times do    # 100 times...
+    die = rand(1..20)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
   erb(:one_twenty)
 end
 # Dice 5/4
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
+  @rolls = [] 
+  5.times do    # 100 times...
+    die = rand(1..4)    # Generate a random number
 
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die}, for a total of #{sum}."
+    @rolls.push(die)    # Add the random number to the array 
+  end
 	
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
